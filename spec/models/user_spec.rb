250050e1cff8ffe6ab67a80dc657dab882e282d5
Your Name <yourname@example.com>
2017-03-14 01:51:24 -0400

    Bookmark/User files: updated, removed expendable code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-   it { is_expected.to have_many(:topics) }
-   it { is_expected.to have_many(:bookmarks) }
-   it { is_expected.to have_many(:likes) }
+  it { is_expected.to have_many(:topics) }
+  it { is_expected.to have_many(:bookmarks) }
+  it { is_expected.to have_many(:likes) }
 
-
-  #  it { is_expected.to validate_presence_of(:name) }
-  #  it { is_expected.to validate_presence_of(:email) }
-  #  it { is_expected.to validate_presence_of(:password) }
   describe "like_for(bookmark)" do
     before do
       @user = FactoryGirl.create(:user)
@@ -17,9 +13,9 @@ RSpec.describe User, type: :model do
       @bookmark = FactoryGirl.create(:bookmark)
     end
 
-    it "denotes whether particular bookmark has been liked" do
-    like = @user.likes.where(bookmark: @bookmark).create
-    expect(@user.liked(@bookmark)).to eq(like)
+    it "denotes whether bookmark has been liked" do
+      like = @user.likes.where(bookmark: @bookmark).create
+      expect(@user.liked(@bookmark)).to eq(like)
+    end
   end
-end
 end
